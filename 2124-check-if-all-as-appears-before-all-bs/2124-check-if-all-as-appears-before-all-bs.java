27ec55e5a32ad1d0bf41c8ca0610eb7d5412087b
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2022-10-10 17:10:34 +0530

    Time: 1 ms (83.92%), Space: 42.4 MB (25.21%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean checkString(String s) {
-     return !s.contains("ba");
+     //return !s.contains("ba");
+   for(int i=1; i<s.length();++i){
+     if(s.charAt(i-1)=='b' && s.charAt(i)=='a') return false;
+   }
+      return true;
     }
 }
