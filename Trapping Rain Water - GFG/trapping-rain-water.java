b169dd857f0e57d58520be2d088e93a13859f2a6
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2023-01-23 17:07:53 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.java
+++ Trapping Rain Water - GFG/trapping-rain-water.java
@@ -38,51 +38,25 @@ class Solution{
     // arr: input array
     // n: size of array
     // Function to find the trapped water between the blocks.
-    static long trappingWater(int a[], int n) { 
+    static long trappingWater(int arr[], int n) { 
+     int left=0,right=n-1;
+     long water=0;
      
-     int left[]= new int[n];
-     int right[]=new int[n];
+     int maxLeft=arr[left];
+     int maxRight=arr[right];
      
-     left[0]=a[0];
-     for(int i=1;i<n;i++){
-         left[i]=Math.max(left[i-1],a[i]);
+     while(left < right){
+         if(arr[left]<=arr[right]){
+             left++;
+             maxLeft=Integer.max(maxLeft,arr[left]);
+             water+=(maxLeft-arr[left]);
+         }else{
+             right--;
+             maxRight=Integer.max(maxRight,arr[right]);
+             water+=(maxRight-arr[right]);
+         }
      }
-     
-     right[n-1]=a[n-1];
-     for(int i=n-2;i>=0;i--){
-         right[i]=Math.max(right[i+1],a[i]);
-     }
-     
-     long ans=0;
-     for(int i=0;i<n;i++){
-         ans=ans+(Math.min(left[i],right[i])-a[i]);
-     }
-     return ans;
-    
-    //  int i =0;
-    //     int j = n-1;
-    //     int lb = 0;
-    //     int rb =0;
-    //     long water =0;
-    //     while(i<j){
-    //         if(arr[i]<=arr[j]){
-    //             if(lb>arr[i]){
-    //                 water+=(lb-arr[i]);
-    //             }else{
-    //                 lb = arr[i];
-    //             }
-    //             i++;
-    //         }else{
-    //             if(rb<arr[j]){
-    //                 rb = arr[j];
-    //             }else{
-    //                 water+=(rb-arr[j]);
-    //             }
-    //             j--;
-    //         }
-    //     }
-    //     return water;
-    } 
+      return water;
 }
-
+}
 
